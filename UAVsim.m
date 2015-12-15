10fe95b85a79f896950e57752d44c37cff7b350f
mcseyyy <ai12820@bristol.ac.uk>
2015-12-15 13:47:17 +0000

    using Runge-Kutta for turning now
    woo

--- UAVsim.m
+++ UAVsim.m
@@ -43,19 +43,20 @@ classdef UAVsim < handle
             %2-SEARCH_LR;
             %3-FOLOW edge (hopefully)
             %4-
+            ang_change = 0;
             if (uav.state==1)
                 if p>0.3
                     uav.d_ang = pi/20;
                 end
                 if p<0.41 %still looking for a cloud
                     fprintf('here\n');
-                    uav.ang = uav.ang + uav.d_ang;
+                    ang_change = uav.d_ang;
                     uav.d_ang = uav.d_ang*0.99;    
                 else %got to a cloud;
                     fprintf('there\n');
                     uav.state = 2; %search_lr
                     uav.d_ang = pi/5;
-                    uav.ang = uav.ang+uav.d_ang;
+                    ang_change = pi/5;
                 end
             elseif (uav.state == 2)
                 if (uav.p>p)
@@ -68,19 +69,37 @@ classdef UAVsim < handle
                 if p<0.3
                     uav.state=1;
                     uav.d_ang = pi/20;
-                elseif p<uav.p
-                    uav.ang = uav.ang-pi/4*uav.clockwise;
+                elseif p<uav.p && p>0.3
+                    %uav.ang = uav.ang+pi/4*uav.clockwise;
+                    ang_change = pi/4*uav.clockwise
                 else
-                    uav.ang = uav.ang+pi/4*uav.clockwise;
+                    %uav.ang = uav.ang-pi/4*uav.clockwise;
+                    ang_change = -pi/4*uav.clockwise;
                 end
             end
-               
+             
+            ang_change = ang_change/uav.speed;
+            x = [uav.x;uav.y; uav.ang];
+            u = [uav.speed; ang_change];
             
-            uav.x = uav.x + uav.speed * sin(uav.ang) * dt;
-            uav.y = uav.y + uav.speed * cos(uav.ang) * dt;
+            k1 = f(x,u);
+            k2 = f(x+k1*dt/2,u);
+            k3 = f(x+k2*dt/2,u);
+            k4 = f(x+k3*dt,u);
+            xnew = x+(k1 + 2*k2 + 2*k3 + k4)*dt/6;
+            uav.x = xnew(1);
+            uav.y = xnew(2);
+            uav.ang = xnew(3);
+       
+            
+            
+            %uav.x = uav.x + uav.speed * sin(uav.ang) * dt;
+            %uav.y = uav.y + uav.speed * cos(uav.ang) * dt;
             uav.p = p;
             fprintf('state=%d speed=%d ang=%f\n',uav.state,uav.speed,uav.ang);
         end
+        
+        
     end
     
 end
---
+++ f.m
@@ -0,0 +1,3 @@
+function xdot = f(x,u)
+    xdot = [u(1)*sin(x(3));u(1)*cos(x(3));u(1)*u(2)];
+end
