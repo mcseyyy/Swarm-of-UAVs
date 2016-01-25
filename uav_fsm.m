aa171de926f2e527bcdc7cf29b8acf737ed9ef7c
mcseyyy <ai12820@bristol.ac.uk>
2016-01-25 01:44:12 +0000

    They talk; they're alive

--- uav_fsm.m
+++ uav_fsm.m
@@ -1,27 +1,51 @@
-function ang_change = uav_fsm(uav,p,dt)
+function ang_change = uav_fsm(uav,p,dt,messages)
     ang_change = 0;
     uav.curr_x_est = uav.get_x();
     uav.curr_y_est = uav.get_y();
     switch uav.state
         case -1,
-             if pdist([uav.x_target,uav.curr_x_est; uav.y_target,uav.curr_y_est],'euclidean')<30
-                fprintf('Reached Targer\n');
+             if pdist([uav.x_target,uav.y_target; uav.curr_x_est ,uav.curr_y_est],'euclidean')<100
+                uav.state = uav.return_state;
              end
+             
              target_ang = atan2(uav.x_target - uav.curr_x_est,  uav.y_target - uav.curr_y_est);
              ang_change = target_ang-uav.ang_est;
         case 0,
+            
             if uav.round_no==3
                 uav.ang_est = atan2(uav.curr_x_est, uav.curr_y_est);
-                uav.state = -1;
+                uav.state = 2;
             else
                 uav.round_no = uav.round_no+1;
             end
         case 1,
-            if p<0.4 %still looking for a cloud
+            for i=1:size(messages,1)
+                if messages(i,3)>0.4
+                    uav.state = -1
+                    uav.x_target = messages(i,1);
+                    uav.y_target = messages(i,2);
+                    uav.return_state = 3;
+                    break;
+                end
+            end
+            
+                    
+            if uav.state ==-1
+                target_ang = atan2(uav.x_target - uav.curr_x_est,  uav.y_target - uav.curr_y_est);
+                ang_change = target_ang-uav.ang_est;
+            elseif (abs(uav.curr_x_est)>900 || abs(uav.curr_y_est) > 900)
+                %if a boundary is reachedbefore finding the cloud,
+                %go back to (0,0) and start searching again
+                uav.state = -1;
+                uav.return_state = 1;
+                uav.spiral_change = pi/20;
+                uav.x_target = 0;
+                uav.y_target = 0;
+            elseif p<0.4 %still looking for a cloud
                 ang_change = uav.spiral_change;
                 uav.spiral_change = uav.spiral_change*0.99;
             else %got to a cloud;
-                uav.state = -1;
+                uav.state = 2;
                 uav.spiral_change = pi/20;
             end
         case 2,
@@ -45,10 +69,13 @@ function ang_change = uav_fsm(uav,p,dt)
                 ang_change = -pi/10;
             end
     end
+    
     ang_change = ang_threshold(ang_change, uav.speed,dt);
+    
         
         
     %estimate the future location of the UAV
+    fprintf('+++++ state=%d id=%d\n',uav.state, uav.id);
     [est_x,est_y,new_ang] = update_location(uav.curr_x_est, uav.curr_y_est, uav.ang_est, uav.speed, ang_change/uav.speed,dt);
     
     %if the future location is outside the map, 
@@ -63,7 +90,7 @@ function ang_change = uav_fsm(uav,p,dt)
         ang_change = pi+pi/20 - uav.ang_est;
     end
     ang_change = ang_threshold(ang_change, uav.speed,dt);
-    fprintf('ang_est=%f\n\n',uav.ang_est/pi*180);
+    
     
     uav.ang_est = mod(uav.ang_est+ang_change+2*pi,2*pi);
     
